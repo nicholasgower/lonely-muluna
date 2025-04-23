050eac2de76c41f24ed9ad1918e2593a22bedd2f
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-23 15:10:55 -0700

    Final touches

--- control.lua
+++ control.lua
@@ -1,13 +1,15 @@
-local function init_storage() 
-    if not storage.respawn_location then storage.respawn_location = {} end
-end
+-- local function init() 
+--     assert(game.is_multiplayer(),"This version of Lonely Muluna can only be played in Multiplayer.")
+--     --if not storage.respawn_location then storage.respawn_location = {} end
+-- end
 
 
-script.on_init(init_storage)
+script.on_init(init)
 
 --/c __lonely-muluna__ storage.respawn_location = {}
 script.on_event(defines.events.on_surface_created, function(event)
     local surface = game.surfaces[event.surface_index]
+    assert(game.is_multiplayer(),"This version of Lonely Muluna can only be played in Multiplayer. Play at https://getcomfy.eu")
     --if surface.name == TENEBRIS then
         --surface.freeze_daytime = true
         --surface.daytime = 0.35
