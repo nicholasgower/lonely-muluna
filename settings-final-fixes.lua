f0dc8af6d262f7c15720717f97f9bccf50b23037
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-28 16:15:17 -0700

    fix: Unhid per-user mod settings.

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -1,6 +1,6 @@
 for _,type in pairs(data.raw) do --Sets all visible settings to their default value and hides them.
     for _,setting in pairs(type) do
-        if setting.hidden == false or setting.hidden == nil then
+        if (setting.hidden == false or setting.hidden == nil) and setting.setting_type ~= "runtime-per-user" then
             setting.hidden = true
             setting.forced_value = setting.default_value
         end
